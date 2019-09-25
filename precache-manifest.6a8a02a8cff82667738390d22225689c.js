428182f5c683f8b9292b4bda3883c8f220cef19c
AhmadKdd <AhmadKdd>
2019-09-24 20:45:45 -0400

    Updates

---
+++ precache-manifest.6a8a02a8cff82667738390d22225689c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf99aa4c30101a50b6f32e4259dfd2ea",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "5aa994d8001f3593b6d9",
+    "url": "/random-quote-machine/static/css/main.5b4687b2.chunk.css"
+  },
+  {
+    "revision": "26bfad54ee845e1b8f69",
+    "url": "/random-quote-machine/static/js/2.62c8d6d1.chunk.js"
+  },
+  {
+    "revision": "5aa994d8001f3593b6d9",
+    "url": "/random-quote-machine/static/js/main.f0d38995.chunk.js"
+  },
+  {
+    "revision": "4240d779d2d64e983759",
+    "url": "/random-quote-machine/static/js/runtime-main.e8014eef.js"
+  }
+]);
